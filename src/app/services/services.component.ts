7fd4714f3149e83b4ca82ba274459d7f19442717
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-04-23 20:08:23 -0300

    up

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent {
+  
+
 
 }
---
+++ src/app/services/services.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicesService {
+
+  private readonly cursos:string[] = ['Angular 18', 'Java', 'Hana', 'Sap'];
+
+  getCursos() {
+    return this.cursos;
+  }
+  
+}
