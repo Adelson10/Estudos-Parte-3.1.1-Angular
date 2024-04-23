cd11493cde615446f817b0b1f13f0bbb9a93c1f7
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-04-23 00:21:18 -0300

    up

--- src/app/diretivas/highlight.directive.ts
+++ src/app/diretivas/highlight.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnInit{
 
-  constructor() { }
+  @HostListener('mouseenter')
+  onMouseOver(){
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave(){
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostBinding('style.backgroundColor') backgroundColor?: string;
+
+  @Input()
+  defaultColor: string = 'white';
+  @Input('appHighlight')
+  highlightColor: string = 'yellow';
   
+  ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
+  }
 }
